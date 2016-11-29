079ad287062449ec45dd07fc18cf4b5ae4d73d61
Fred <helmesjo@live.com>
2016-11-29 18:32:17 +0100

    Fixed tests and implementation of grid.getAdjacent(x, y)

--- src/grid.h
+++ src/grid.h
@@ -11,9 +11,15 @@ namespace helmesjo {
 
 		grid(size_t width, size_t height);
 
-		decltype(auto) get(size_t x, size_t y);
+		void set(size_t x, size_t y, T& obj);
+		decltype(auto) get(size_t x, size_t y) const;
+		std::vector<T> getAdjacent(size_t x, size_t y) const;
+
+		decltype(auto) begin() const;
+		decltype(auto) end() const;
 
 	private:
+
 		const size_t width = 0, height = 0;
 		std::vector<T> elements;
 	};
@@ -28,7 +34,13 @@ namespace helmesjo {
 	};
 	const std::array<direction, 8> directions = { { { -1,-1 } ,{ -1,0 } ,{ -1,1 } ,{ 0,-1 } ,{ 0,1 } ,{ 1,-1 } ,{ 1,0 } ,{ 1,1 } } };
 
-	static int rowBasedIndex(unsigned int width, unsigned int x, unsigned int y) {
+	static bool isInRange(size_t width, size_t height, int x, int y)
+	{
+		return x >= 0 && x < static_cast<int>(width) && y >= 0 && y < static_cast<int>(height);
+	}
+
+	static int rowBasedIndex(size_t width, size_t x, size_t y)
+	{
 		return width * y + x;
 	}
 
@@ -41,9 +53,41 @@ namespace helmesjo {
 	}
 
 	template<typename T>
-	inline decltype(auto) helmesjo::grid<T>::get(size_t x, size_t y)
+	inline void grid<T>::set(size_t x, size_t y, T & obj)
+	{
+		auto index = rowBasedIndex(width, x, y);
+		elements[index] = obj;
+	}
+	template<typename T>
+	inline decltype(auto) helmesjo::grid<T>::get(size_t x, size_t y) const
 	{
 		auto index = rowBasedIndex(width, x, y);
 		return elements[index];
+	}
+	template<typename T>
+	inline std::vector<T> grid<T>::getAdjacent(size_t x, size_t y) const
+	{
+		auto adjacent = std::vector<T>();
+		for (auto dir : directions) {
+			int dx = x + dir.x;
+			int dy = y + dir.y;
+
+			if (isInRange(width, height, dx, dy)) {
+				auto ajdElement = get(dx, dy);
+				//if ((included & ajdElement.state) == ajdElement.state)
+				adjacent.push_back(ajdElement);
+			}
+		}
+		return adjacent;
+	}
+	template<typename T>
+	inline decltype(auto) grid<T>::begin() const
+	{
+		return elements.cbegin();
+	}
+	template<typename T>
+	inline decltype(auto) grid<T>::end() const
+	{
+		return elements.cend();
 	}
 }
--- test/grid_tests.cpp
+++ test/grid_tests.cpp
@@ -2,23 +2,70 @@
 
 #include <catch.hpp>
 
+using namespace helmesjo;
 
 SCENARIO("Access elements", "[grid]") {
+	using Type = int;
 
+	GIVEN("a 2x2 grid") {
+		auto myGrid = grid<Type>(2, 2);
+
+		WHEN("element (1,0) is requested") {
+			auto element = myGrid.get(1, 0);
+
+			THEN("a default constructed T is returned") {
+
+				REQUIRE(element == Type());
+			}
+		}
+	}
+	GIVEN("a 3x3 grid") {
+		auto myGrid = grid<Type>(3, 3);
+
+		WHEN("adjacent elements are requested for element (1,1)") {
+			auto adjacent = myGrid.getAdjacent(1, 1);
+
+			THEN("all 8 surrounding elements should be returned") {
+
+				REQUIRE(adjacent.size() == 8);
+			}
+		}
+
+		WHEN("adjacent elements are requested for element (2,2)") {
+			auto adjacent = myGrid.getAdjacent(0, 0);
+
+			THEN("3 adjacent elements should be returned") {
+
+				REQUIRE(adjacent.size() == 3);
+			}
+		}
+
+		WHEN("adjacent elements are requested for element (1,1)") {
+			auto adjacent = myGrid.getAdjacent(1, 0);
+
+			THEN("5 adjacent elements should be returned") {
+
+				REQUIRE(adjacent.size() == 5);
+			}
+		}
+	}
+}
+
+SCENARIO("Modify grid") {
 	GIVEN("a 2x2 grid") {
 		using Type = int;
 		auto grid = helmesjo::grid<Type>(2, 2);
 
-		WHEN("element (1,0) is requested") {
-			grid.get(1, 0) = 10;
+		WHEN("element (0,1) is assigned a new value") {
+			unsigned int x = 0, y = 1;
+			Type newVal = 10;
+			grid.set(x, y, newVal);
 
-			THEN("a default constructed T is returned") {
+			THEN("it changes the actual element in the grid") {
+				auto gridVal01 = grid.get(x, y);
 
-				//REQUIRE(element == Type());
+				REQUIRE(gridVal01 == newVal);
 			}
-
 		}
-
 	}
-
 }
